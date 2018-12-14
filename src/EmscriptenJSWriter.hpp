86e3171bd3e4986218d4cc5e332b481ed866cbbf
Bumsik Kim <kbumsik@gmail.com>
2018-12-14 21:56:49 +0900

    WebMOpus now stores stream well, but there is timestamp problem

--- src/EmscriptenJSWriter.hpp
+++ src/EmscriptenJSWriter.hpp
@@ -5,8 +5,10 @@
 #include <cstdint>
 
 EM_JS(void, queueBuffer, (const void* buf, int len), {
+  // Create a Typed Array
   let array = new Uint8Array(Module.HEAPU8.buffer, buf, len);
-  webmOpusWorker.encodedBuffers.push(array.buffer);
+  // Then copy and queue
+  webmOpusWorker.encodedBuffers.push(new Uint8Array(array).buffer);
 });
 
 #endif /* EMSCRIPTEN_JS_WRITER_H_ */
