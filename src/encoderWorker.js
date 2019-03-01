1a97a60018d78d7e29b3a2291bfb3f9678ffee59
Bumsik Kim <kbumsik@gmail.com>
2019-03-01 20:06:52 +0900

    Better modularization for encoderWorker

--- src/encoderWorker.js
+++ src/encoderWorker.js
@@ -1,104 +1,95 @@
-'use strict';
+function initWorker (workerGlobalScope) {
+  const WaveEncoder = require('./WaveEncoder.js');
+  const WebMOpusEncoder = require('./WebMOpusEncoder.js');
+  const OggOpusEncoder = require('./OggOpusEncoder.js');
 
-const WaveEncoder = require('./WaveEncoder.js');
-const WebMOpusEncoder = require('./WebMOpusEncoder.js');
-const OggOpusEncoder = require('./OggOpusEncoder.js');
+  let encoder;
 
-let encoder;
+  workerGlobalScope.onmessage = function (e) {
+    const { command } = e.data;
+    switch (command) {
+      case 'loadEncoder':
+        const { mimeType, wasmPath } = e.data;
+        // Setting encoder module
+        let encoderModule;
+        switch (mimeType) {
+          case 'audio/wav':
+          case 'audio/wave':
+            encoderModule = WaveEncoder;
+            break;
 
-self.onmessage = function (e) {
-  const { command } = e.data;
-  switch (command) {
-    case 'loadEncoder':
-      const { mimeType, wasmPath } = e.data;
-      // Setting encoder module
-      let encoderModule;
-      switch (mimeType) {
-        case 'audio/wav':
-        case 'audio/wave':
-          encoderModule = WaveEncoder;
-          break;
+          case 'audio/webm':
+            encoderModule = WebMOpusEncoder;
+            break;
 
-        case 'audio/webm':
-          encoderModule = WebMOpusEncoder;
-          break;
+          case 'audio/ogg':
+            encoderModule = OggOpusEncoder;
+            break;
+        }
+        // Override Emscripten configuration
+        let moduleOverrides = {};
+        if (wasmPath) {
+          moduleOverrides['locateFile'] = function (path, scriptDirectory) {
+            return path.match(/.wasm/) ? wasmPath : (scriptDirectory + path);
+          };
+        }
+        // Initialize the module
+        encoderModule(moduleOverrides).then(Module => {
+          encoder = Module;
+          // Notify the host ready to accept 'init' message.
+          self.postMessage({ command: 'readyToInit' });
+        });
+        break;
 
-        case 'audio/ogg':
-          encoderModule = OggOpusEncoder;
-          break;
-      }
-      // Override Emscripten configuration
-      let moduleOverrides = {};
-      if (wasmPath) {
-        moduleOverrides['locateFile'] = function (path, scriptDirectory) {
-          return path.match(/.wasm/) ? wasmPath : (scriptDirectory + path);
-        };
-      }
-      // Initialize the module
-      encoderModule(moduleOverrides).then(Module => {
-        encoder = Module;
-        // Notify the host ready to accept 'init' message.
-        self.postMessage({ command: 'readyToInit' });
-      });
-      break;
+      case 'init':
+        const { sampleRate, channelCount, bitsPerSecond } = e.data;
+        encoder.init(sampleRate, channelCount, bitsPerSecond);
+        break;
 
-    case 'init':
-      const { sampleRate, channelCount, bitsPerSecond } = e.data;
-      encoder.init(sampleRate, channelCount, bitsPerSecond);
-      break;
+      case 'pushInputData':
+        const { channelBuffers, length, duration } = e.data; // eslint-disable-line
+        // On Chrome, Float32Array doesn't recognize its buffer after transferred.
+        // So re-create Float32Array right after a web worker received it.
+        for (let i = 0; i < channelBuffers.length; i++) {
+          channelBuffers[i] = new Float32Array(channelBuffers[i].buffer);
+        }
 
-    case 'pushInputData':
-      const { channelBuffers, length, duration } = e.data; // eslint-disable-line
-      // On Chrome, Float32Array doesn't recognize its buffer after transferred.
-      // So re-create Float32Array right after a web worker received it.
-      for (let i = 0; i < channelBuffers.length; i++) {
-        channelBuffers[i] = new Float32Array(channelBuffers[i].buffer);
-      }
+        encoder.encode(channelBuffers);
+        break;
 
-      encoder.encode(channelBuffers);
-      break;
+      case 'getEncodedData':
+      case 'done':
+        if (command === 'done') {
+          encoder.close();
+        }
 
-    case 'getEncodedData':
-    case 'done':
-      if (command === 'done') {
-        encoder.close();
-      }
+        const buffers = encoder.flush();
+        self.postMessage({
+          command: command === 'done' ? 'lastEncodedData' : 'encodedData',
+          buffers
+        }, buffers);
 
-      const buffers = encoder.flush();
-      self.postMessage({
-        command: command === 'done' ? 'lastEncodedData' : 'encodedData',
-        buffers
-      }, buffers);
+        if (command === 'done') {
+          self.close();
+        }
+        break;
 
-      if (command === 'done') {
-        self.close();
-      }
-      break;
+      default:
+        // Ignore
+        break;
+    }
+  };
+}
 
-    default:
-      // Ignore
-      break;
-  }
-};
+/* global WorkerGlobalScope */
+// Run only if it is in web worker environment
+if (typeof WorkerGlobalScope !== 'undefined' &&
+    self instanceof WorkerGlobalScope) {
+  initWorker(self);
+}
 
 /**
- * This part is used by wrapper-webpack-plugin when bundled. This is done to
- * make sure the web worker works both by importing <script> tag and by using
- * a bundler(e.g. webpack, rollup). See webpack.worker.config.js
+ * TODO: This line causes undefined symbol: __webpack_require__
+ * So comment out until figuring out the solution
  */
-// /* global WorkerGlobalScope */
-// (function () {
-//   function initWorker () {
-//     // Whole webpack output goes here
-//   }
-
-//   if (typeof window !== 'undefined' &&
-//       typeof window.OpusMediaRecorder === 'function') {
-//     // If the script is imported by using <script> tag
-//     window.OpusMediaRecorder.encoderWorker = initWorker;
-//   } else if (typeof WorkerGlobalScope !== 'undefined' &&
-//              self instanceof WorkerGlobalScope) {
-//     // If it is in web worker environment
-//     initWorker();
-//   }
-// })();
+// module.exports = initWorker;
--- webpack.worker.config.js
+++ webpack.worker.config.js
@@ -1,46 +1,7 @@
-const WrapperPlugin = require('wrapper-webpack-plugin');
-
-/**
- * The purpose of this function is to create the header and footer for IIFEed
- * the webpack output of encorderWorker.js.
- */
-/* global window */
-/* global WorkerGlobalScope */
-let encoderWorkerIIFEWrapper = function () {
-  function initWorker () {
-    // Whole webpack output goes here
-  }
-  if (typeof window !== 'undefined' &&
-      typeof window.OpusMediaRecorder === 'function') {
-    // If the script is imported by using <script> tag
-    window.OpusMediaRecorder.encoderWorker = initWorker;
-  } else if (typeof WorkerGlobalScope !== 'undefined' &&
-             self instanceof WorkerGlobalScope) {
-    // If it is in web worker environment
-    initWorker();
-  }
-};
-let wrapperStr = encoderWorkerIIFEWrapper.toString();
-
 module.exports = {
-  plugins: [
-    new WrapperPlugin({
-      afterOptimizations: true,
-      test: /\.js$/, // only wrap output of bundle files with '.js' extension
-      // (function () { function initWorker (){
-      header: '(' +
-              wrapperStr.substr(
-                0, wrapperStr.indexOf(
-                  '{', wrapperStr.indexOf('initWorker')) + 1) +
-              '\n',
-      //   } if (typeof window !== 'undefined'...
-      //   ...
-      // })();
-      footer: '\n' +
-              wrapperStr.substr(wrapperStr.indexOf('}')) +
-              ')();'
-    })
-  ],
+  output: {
+    globalObject: 'typeof OpusMediaRecorder !== \'undefined\' ? OpusMediaRecorder : typeof self !== \'undefined\' ? self : this'
+  },
   entry: {
     encoderWorker: './src/encoderWorker.js'
   },
