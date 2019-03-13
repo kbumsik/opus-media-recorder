2e581790961ea29177b6f3006e4020b57ecfd66b
Bumsik Kim <k.bumsik@gmail.com>
2019-03-13 14:26:56 +0900

    Version bump: 0.7.17

--- docs/example.js
+++ docs/example.js
@@ -2,8 +2,8 @@
 
 // Non-standard options
 const workerOptions = {
-  OggOpusEncoderWasmPath: 'https://cdn.jsdelivr.net/npm/opus-media-recorder@0.7.16/OggOpusEncoder.wasm',
-  WebMOpusEncoderWasmPath: 'https://cdn.jsdelivr.net/npm/opus-media-recorder@0.7.16/WebMOpusEncoder.wasm'
+  OggOpusEncoderWasmPath: 'https://cdn.jsdelivr.net/npm/opus-media-recorder@0.7.17/OggOpusEncoder.wasm',
+  WebMOpusEncoderWasmPath: 'https://cdn.jsdelivr.net/npm/opus-media-recorder@0.7.17/WebMOpusEncoder.wasm'
 };
 
 // Polyfill MediaRecorder
